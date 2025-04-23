8921dc317ea9456ac9e705ad0b97c97bba355c95
thakur156 <aliisrar156@gmail.com>
2025-04-23 22:36:39 +0900

    feat: implement file upload functionality with multer and integrate Cloudinary for image storage

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,12 +6,13 @@ import {
   updateProfile,
 } from "../controllers/user.controller.js";
 import { isAuthenticated } from "../middleware/isAuthenticated.js";
-import { singleUpload } from "../middleware/multer.js";
+import { upload } from "../middleware/multer.js";
+
 const router = express.Router();
 
-router.post("/signup", singleUpload, signup);
+router.post("/signup", upload.single("file"), signup);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
-router.put("/update", isAuthenticated, singleUpload, updateProfile);
+router.put("/update", isAuthenticated, upload.single("file"), updateProfile);
 
 export default router;
